83017e2409a0d0479a07e5e88ca6a08ffc3a4b87
XarisPas <xaris_pasch@hotmail.com>
2017-05-03 14:22:00 +0300

    remove code in comments

--- assets/expandtext.js
+++ assets/expandtext.js
@@ -17,22 +17,4 @@
       }
     });
   }
-    
-    /*var expandButtonss = $('.tab-content .expand a#two');
-  if (expandButtonss.length>0) {
-    var expandButtons = expandButtonss.eq(0);
-    var expandButtonTexts = expandButtons.text()+'<span class="downIcon"></span>';
-    var expandButtonDescriptions = expandButtons.attr('data-description')+'<span class="upIcon"></span>';
-    expandButtons.attr('href', 'javascript:');
-    $('.expand a#two').bind('click', function(e) {
-      var continueArticles = $('.continueArticles');
-      if (continueArticles.css('display')=='none') {
-        continueArticles.show();
-        expandButtons.html(expandButtonDescriptions);
-      } else {
-        continueArticles.hide();
-        expandButtons.html(expandButtonTexts);
-      }
-    });
-  }  */
 });
