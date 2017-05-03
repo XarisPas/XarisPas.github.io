7a8fbadcc3195faa2ec17c7a591131be75825fc5
XarisPas <xaris_pasch@hotmail.com>
2017-05-03 14:35:27 +0300

    add expandstext.js

---
+++ assets/expandstext.js
@@ -0,0 +1,21 @@
+
+  $(document).ready(function() {
+    /* expand text */
+  var expandButtons = $('.tab-content .expands a');
+  if (expandButtons.length>0) {
+    var expandButton = expandButtons.eq(0);
+    var expandButtonText = expandButton.text()+'<span class="downIcon"></span>';
+    var expandButtonDescription = expandButton.attr('data-description')+'<span class="upIcon"></span>';
+    expandButton.attr('href', 'javascript:');
+    $('.expands a').bind('click', function(e) {
+      var continueArticle = $('.continueArticles');
+      if (continueArticle.css('display')=='none') {
+        continueArticle.show();
+        expandButton.html(expandButtonDescription);
+      } else {
+        continueArticle.hide();
+        expandButton.html(expandButtonText);
+      }
+    });
+  }
+});
