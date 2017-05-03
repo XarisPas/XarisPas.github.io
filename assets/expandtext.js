e350e2b411934fa8d821f2cad8a1beb77a91f54d
XarisPas <xaris_pasch@hotmail.com>
2017-05-03 14:30:50 +0300

    add boolean declarations

--- assets/expandtext.js
+++ assets/expandtext.js
@@ -1,6 +1,6 @@
   $(document).ready(function() {
     /* expand text */
-  var expandButtons = $('.tab-content .expand a');
+  var expandButtons = $('.tab-content .expand a' || '.tab-content .expands a' || '.tab-content .expandss a');
   if (expandButtons.length>0) {
     var expandButton = expandButtons.eq(0);
     var expandButtonText = expandButton.text()+'<span class="downIcon"></span>';
@@ -14,26 +14,6 @@
       } else {
         continueArticle.hide();
         expandButton.html(expandButtonText);
-      }
-    });
-  }
-});
-  $(document).ready(function() {
-    /* expand text */
-  var expandButtonss = $('.tab-content .expands a');
-  if (expandButtonss.length>0) {
-    var expandButtons = expandButtonss.eq(0);
-    var expandButtonTexts = expandButtons.text()+'<span class="downIcon"></span>';
-    var expandButtonDescriptions = expandButtons.attr('data-description')+'<span class="upIcon"></span>';
-    expandButtons.attr('href', 'javascript:');
-    $('.expands a').bind('click', function(e) {
-      var continueArticle = $('.continueArticle');
-      if (continueArticle.css('display')=='none') {
-        continueArticle.show();
-        expandButtons.html(expandButtonDescriptions);
-      } else {
-        continueArticle.hide();
-        expandButtons.html(expandButtonTexts);
       }
     });
   }
