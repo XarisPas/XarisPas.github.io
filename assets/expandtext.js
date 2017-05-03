193d3db2a0bd51042a6098f9f4d1e63aa7e98774
XarisPas <xaris_pasch@hotmail.com>
2017-05-03 13:51:34 +0300

    modify code adding a class

--- assets/expandtext.js
+++ assets/expandtext.js
@@ -1,12 +1,12 @@
   $(document).ready(function() {
     /* expand text */
-  var expandButtons = $('.tab-content .expand a');
+  var expandButtons = $('.tab-content .expand a.one');
   if (expandButtons.length>0) {
     var expandButton = expandButtons.eq(0);
     var expandButtonText = expandButton.text()+'<span class="downIcon"></span>';
     var expandButtonDescription = expandButton.attr('data-description')+'<span class="upIcon"></span>';
     expandButton.attr('href', 'javascript:');
-    $('.expand a').bind('click', function(e) {
+    $('.expand a.one').bind('click', function(e) {
       var continueArticle = $('.continueArticle');
       if (continueArticle.css('display')=='none') {
         continueArticle.show();
